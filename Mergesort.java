677f8131b81bcc60b36f237ac8b6aac3984d8bff
SUNKARA PURNA JASWANTH <sunkarapurnajaswanth@gmail.com>
2026-09-10 11:41:53 +0530

    Implement merge sort algorithm in Mergesort.java

---
+++ Mergesort.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Main {
+
+    static void mergeSort(int[] arr, int low, int high) {
+
+        if (low >= high)
+            return;
+
+        int mid = low + (high - low) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+
+    static void merge(int[] arr, int low, int mid, int high) {
+
+        int[] temp = new int[high - low + 1];
+
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= high) {
+
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= high)
+            temp[k++] = arr[j++];
+
+        for (i = low; i <= high; i++)
+            arr[i] = temp[i - low];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        mergeSort(arr, 0, n - 1);
+
+        System.out.println("Sorted array:");
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
